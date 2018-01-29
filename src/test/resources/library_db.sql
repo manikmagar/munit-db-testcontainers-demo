5a869b10a99d77f3ac8dbc4d43824b9a428d66ae
manik.magar <manik.magar@gmail.com>
2018-01-29 00:30:01 -0500

    Updated demo tests

--- src/test/resources/library_db.sql
+++ src/test/resources/library_db.sql
@@ -1,7 +1,5 @@
 drop table if exists books;
 
-drop table if exists transactions;
-
 CREATE TABLE books (
   book_id int(11) NOT NULL AUTO_INCREMENT,
   title varchar(45) NOT NULL,
@@ -13,12 +11,3 @@ CREATE TABLE books (
   PRIMARY KEY (book_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
-CREATE TABLE transactions (
-  transaction_id int(11) NOT NULL AUTO_INCREMENT,
-  book_id int(11) NOT NULL,
-  checkout_time timestamp NOT NULL,
-  checkin_time timestamp NULL,
-  PRIMARY KEY (transaction_id),
-  FOREIGN KEY (book_id) references books(book_id)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
