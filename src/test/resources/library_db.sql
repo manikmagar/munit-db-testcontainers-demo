02a4cca0f9db2b68203566ce526120b2acaad8b5
manik.magar <manik.magar@gmail.com>
2018-02-04 15:17:25 -0500

    Demo with Stored Procedure
    This uses 0.0.2 version of connector and snapshot versions of TestContainers to allow using stored procedures in the init script See Issue #570 on TestContainers.

--- src/test/resources/library_db.sql
+++ src/test/resources/library_db.sql
@@ -11,3 +11,20 @@ CREATE TABLE books (
   PRIMARY KEY (book_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+DROP PROCEDURE IF EXISTS calculate_library_value;
+
+CREATE PROCEDURE `calculate_library_value`(out totalValue decimal)
+BEGIN
+
+	-- Total Value of Library books
+	select sum(price * total_quantity) into totalValue from books;
+	
+	/*
+		Also get all the books.
+	*/
+
+	BEGIN 
+		select * from books order by title;
+	END;
+
+END;
